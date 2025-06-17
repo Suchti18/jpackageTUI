00d44b09638b73a8da3cbcef4eef983389b12ac7
Nils <t@t.com>
2025-06-18 00:22:29 +0200

    Added a functionality to the button

--- cmd/jpackageTUI/main.go
+++ cmd/jpackageTUI/main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"time"
 )
 
+var (
+	app  *tview.Application
+	flex *tview.Flex
+)
+
+func addNewButton() {
+	text := tview.NewTextView().SetText("Hello, world")
+	flex.AddItem(text, 0, 1, false)
+}
+
+func every5Sec() {
+	tick := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <-tick.C:
+			addNewButton()
+		}
+	}
+}
+
 func main() {
-	app := tview.NewApplication()
-	flex := tview.NewFlex()
+	app = tview.NewApplication()
+	app.EnableMouse(true)
+
+	flex = tview.NewFlex()
+	flex.SetBorder(true)
+	flex.SetTitle("jpackageTUI")
 	flex.SetDirection(tview.FlexRow)
 
-	button := tview.NewButton("Add Text").SetSelectedFunc(func() {
-		text := tview.NewTextView().SetText("Hello, world")
-		flex.AddItem(text, 0, 1, false)
-		app.ForceDraw()
-	}).SetLabelColor(tcell.ColorWhite).SetLabelColorActivated(tcell.ColorRed)
+	button := tview.NewButton("Click me")
+	button.SetFocusFunc(func() {
+		addNewButton()
+	})
 
 	flex.AddItem(tview.NewTextView().SetText("Hello, world"), 0, 1, false)
 	flex.AddItem(button, 0, 1, false)
+
+	//go every5Sec()
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
